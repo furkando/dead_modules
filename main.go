53579e636032c6725eef4a1c4d3b1468a388e23c
Furkan Dogan <furkandoganktf@hotmail.com>
2024-07-28 23:20:49 +0300

    Feat: Add version flag

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"dead_modules/ui"
+	"flag"
+	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
+	// Define the --version flag
+	versionFlag := flag.Bool("version", false, "Print the version number and exit")
+	flag.Parse()
+
+	// Check if the --version flag was provided
+	if *versionFlag {
+		fmt.Printf("Dead Modules %s\n", ui.Version)
+		os.Exit(0)
+	}
+
 	if err := ui.StartApp(); err != nil {
 		log.Fatalf("Error running application: %v", err)
 	}
